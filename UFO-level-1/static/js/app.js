48fa9e207034cd1cc84dbac477b07f3462d5d494
Daniel Valdes <53982849+daniel-valdes@users.noreply.github.com>
2020-01-04 20:23:32 -0500

    Pushing updated js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,37 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+data.forEach(function(UFOsighting) {
+    console.log(UFOsighting);
+
+    var row = tbody.append("tr");
+
+    Object.entries(UFOsighting).forEach(function([key, value]) {
+        console.log(key, value);
+
+        // Append a cell to the row for each value
+        // in the weather report object
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(UFOsighting => UFOsighting.datetime === inputValue)
+
+    console.log(filteredData);  
+});
